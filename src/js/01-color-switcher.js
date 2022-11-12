acd23b6df3da2da996d7b9d8fdc0c14bf4aafd61
Vladislav Ulianeev <vladis.ulian@gmail.com>
2022-11-12 20:44:42 +0200

    Color-switcher is done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,12 @@
+function switchColor(interval) {
+  document.body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+console.log('Смена цвета');
 
+const buttons = {
+  startButton: document.querySelector('[data-start]'),
+};
+console.log(buttons.startButton);
+const intervalId = setInterval(switchColor, 1000, 1000);
