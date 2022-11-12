3009f735cbba6e867925446141ead52cbe55d910
vladisulian <106911452+vladisulian@users.noreply.github.com>
2022-11-12 18:45:11 +0000

    Deploying to gh-pages from @ vladisulian/goit-js-hw-09@acd23b6df3da2da996d7b9d8fdc0c14bf4aafd61 🚀

---
+++ 01-color-switcher.0c3e1b5d.js
@@ -0,0 +1,2 @@
+console.log("Смена цвета");const t={startButton:document.querySelector("[data-start]")};console.log(t.startButton);setInterval((function(t){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3,1e3);
+//# sourceMappingURL=01-color-switcher.0c3e1b5d.js.map
---
+++ 01-color-switcher.26b57b15.js
@@ -0,0 +1,2 @@
+!function(){console.log("Смена цвета");var t={startButton:document.querySelector("[data-start]")};console.log(t.startButton);setInterval((function(t){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3,1e3)}();
+//# sourceMappingURL=01-color-switcher.26b57b15.js.map
