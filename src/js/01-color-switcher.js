764067b7ac89b088a92cc2fe36fdbf22e681264e
Vladislav Ulianeev <vladis.ulian@gmail.com>
2022-11-12 20:57:33 +0200

    task 1 is done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,12 +1,26 @@
-function switchColor(interval) {
+const buttons = {
+  startButton: document.querySelector('[data-start]'),
+  endbutton: document.querySelector('[data-stop]'),
+};
+function switchColor() {
   document.body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
-console.log('Смена цвета');
+buttons.endbutton.disabled = true;
 
-const buttons = {
-  startButton: document.querySelector('[data-start]'),
-};
-console.log(buttons.startButton);
-const intervalId = setInterval(switchColor, 1000, 1000);
+let intervalId;
+document.body.style.disabled = true;
+
+buttons.startButton.addEventListener('click', () => {
+  intervalId = setInterval(switchColor, 1000, 1000);
+
+  buttons.startButton.disabled = true;
+  buttons.endbutton.disabled = false;
+});
+buttons.endbutton.addEventListener('click', () => {
+  clearInterval(intervalId);
+
+  buttons.startButton.disabled = false;
+  buttons.endbutton.disabled = true;
+});
