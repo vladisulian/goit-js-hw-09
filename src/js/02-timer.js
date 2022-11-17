6b3f58e8959eb825bc0f7fab317dd3c128f045ba
Vladislav Ulianeev <vladis.ulian@gmail.com>
2022-11-17 19:24:50 +0200

    first step

---
+++ src/js/02-timer.js
@@ -0,0 +1,19 @@
+// Описан в документации
+import flatpickr from 'flatpickr';
+// Дополнительный импорт стилей
+import 'flatpickr/dist/flatpickr.min.css';
+
+const flatpickr = require('flatpickr');
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      console.log(selectedDates[0]);
+    },
+  };
+flatpickr('#datetime-picker', options);
+
+
