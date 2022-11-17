587d7feb6644b9ebc52933cfa49cf43094ea0ea0
Vladislav Ulianeev <vladis.ulian@gmail.com>
2022-11-17 23:21:46 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -6,14 +6,21 @@ import 'flatpickr/dist/flatpickr.min.css';
 const flatpickr = require('flatpickr');
 
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },
-  };
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const choosenDate = selectedDates[0];
+    console.log(choosenDate);
+    if (choosenDate < new Date()) {
+      alert('Choose correct date, please');
+      const input = document.querySelector('#datetime-picker');
+      console.log(input.value);
+      return;
+    }
+  },
+};
 flatpickr('#datetime-picker', options);
 
-
+console.log(new Date());
