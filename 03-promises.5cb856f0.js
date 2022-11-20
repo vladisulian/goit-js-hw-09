28213a4ffb137a6a2d112744cffa6e6f39cf80ed
vladisulian <106911452+vladisulian@users.noreply.github.com>
2022-11-20 17:02:55 +0000

    Deploying to gh-pages from @ vladisulian/goit-js-hw-09@44456300ccabee320d9d148ead99b7216d2ae5fd 🚀

--- 03-promises.5cb856f0.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("form").addEventListener("submit",(position,delay,void Math.random()));
-//# sourceMappingURL=03-promises.5cb856f0.js.map
--- 03-promises.8e468d2a.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("form").addEventListener("submit",(position,delay,void Math.random()));
-//# sourceMappingURL=03-promises.8e468d2a.js.map
